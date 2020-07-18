c0e518e5ad1b5621abc1f14e203427b97b479d8f
Dhruvil Gajjar <dgajjar999@gmail.com>
2020-07-18 23:23:42 +0530

    add header

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "../api/axios";
+import requests from "../api/requests";
+import "../style/banner.css";
 
 function Banner() {
-  return <div></div>;
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOrigianls);
+      setMovies(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <header
+      className='banner'
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movies?.backdrop_path}")`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className='banner_contents'>
+        <h1>{movies?.title || movies?.name || movies?.original_name}</h1>
+
+        <div className='banner_buttons'>
+          <button className='banner_button'>Play</button>
+          <button className='banner_button'>My List</button>
+        </div>
+
+        <h1 className='banner_description'>{movies?.overview}</h1>
+      </div>
+    </header>
+  );
 }
 
 export default Banner;

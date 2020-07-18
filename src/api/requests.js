f87be367634f9f440138a9fee85e700cddbd9679
Dhruvil Gajjar <dgajjar999@gmail.com>
2020-07-18 18:25:15 +0530

    get movie poster

--- src/api/requests.js
+++ src/api/requests.js
@@ -1,8 +1,8 @@
 const API_KEY = "9f9c59c1da7d67cf4e663ed7dce50af8";
 
 const requests = {
-  fetchTrending: `/tranding/all/week/?api_key=${API_KEY}&language=en-US`,
-  fetchNetflixOrigianls: `/discover/tv?api_key=${API_KEY}with_networks=213`,
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOrigianls: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
   fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
   fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,

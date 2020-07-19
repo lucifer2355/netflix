e8a49b97e6e3ef74f51f707ee4c095da6bd41abb
Dhruvil Gajjar <dgajjar999@gmail.com>
2020-07-19 16:44:15 +0530

    add nav bar css

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "../style/nav.css";
 
 function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
   return (
-    <div className='nav'>
+    <div className={`nav ${show && "nav__black"}`}>
       <img
         className='nav__logo'
         src='https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.svg'

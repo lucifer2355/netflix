e531864dc3ceabac82b7f73dc2b92f1a46dbe6e1
Dhruvil Gajjar <dgajjar999@gmail.com>
2020-07-19 16:15:53 +0530

    add nav bar component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import Row from "./components/Row";
 import requests from "./api/requests";
 import Banner from "./components/Banner";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className='app'>
+      <Nav />
       <Banner />
       <Row
         title='NETFLIX ORIGINALS'
